ea7d4c5c0543f32a9a939a99e877ef2c1554e490
r3-3s <rees.maguire@gmail.com>
2025-02-18 13:38:39 +1100

    Add files via upload

---
+++ advanced_data_types.py
@@ -0,0 +1,16 @@
+mycolourlist = ["red", "blue", "green"]
+mycolourlist.append ("yellow")
+print(mycolourlist)
+#The above is testing lists
+
+#The below is an attempt at a tuple
+mytuple = ("1", "3", "2")
+print(mytuple)
+#Tuples are unchangable.
+
+#The below is an attempt at a set
+setnumbers = {"10", "20", "30", "40"}
+setnumbers.add(50)
+print(setnumbers)
+
+
---
+++ operators_demo.py
@@ -0,0 +1,18 @@
+print(3+3) #Output = 6
+print(3*3) #Output = 9
+print(3==3) #Output = True
+print(3==4) #Output = False
+print(3!=4) #Output = True
+
+x=6
+print(x==6) #Output = True
+print(x%2) #Output = 0 
+print(x-+3) #Output = 3
+
+x=7
+y=1
+if (x <= y):
+    print("Incorrect")
+else:
+    print("correct")
+    
